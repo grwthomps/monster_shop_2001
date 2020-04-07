4f278980449396794083a46d2177012451758208
Graham Thompson <grwthomps@gmail.com>
2020-04-07 12:16:06 -0600

    Silence all deprecation warnings when running tests in terminal

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 SimpleCov.start
 
+# Silence all warnings
+$VERBOSE=nil
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
